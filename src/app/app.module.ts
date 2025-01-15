2d5841d28819d16ff7a40c84ac4790a8d4ff1a1c
pavan shidole <pavanshidole@gmail.com>
2025-01-15 11:27:59 +0530

    work on todo and student service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoFormComponent } from './shared/components/todo-form/todo-form.component';
+import { TodoListComponent } from './shared/components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoFormComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
